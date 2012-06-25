797e964f93acfdcc5052ae9ea9241a7ba1be2222
Jonas Baumann <jone@jone.ch>
2012-06-25 10:14:58 +0200

    Fix display settings test.

--- ftw/simplelayout/tests/test_display_settings.py
+++ ftw/simplelayout/tests/test_display_settings.py
@@ -1,11 +1,10 @@
-from ftw.simplelayout.testing import SIMPLELAYOUT_ZCML_LAYER
 from ftw.simplelayout.interfaces import IDisplaySettings
+from ftw.simplelayout.testing import SIMPLELAYOUT_ZCML_LAYER
 from ftw.testing import MockTestCase
-from mocker import ANY
+from zope.annotation import IAttributeAnnotatable
 from zope.component import getMultiAdapter
-from zope.interface.verify import verifyClass
 from zope.component import queryMultiAdapter
-from zope.annotaions import IAttributeAnnotable
+from zope.interface.verify import verifyClass
 
 
 class TestDisplaySettings(MockTestCase):
@@ -13,28 +12,32 @@ class TestDisplaySettings(MockTestCase):
     layer = SIMPLELAYOUT_ZCML_LAYER
 
     def setUp(self):
-        MockTestCase.setUp(self)
+        super(TestDisplaySettings, self).setUp()
 
-        self.context = self.proveding_stub(IAttributeAnnotable)
+        self.context = self.providing_stub(IAttributeAnnotatable)
         self.request = self.stub_request()
 
     def test_component_registered(self):
-        component = queryMultiAdapter((self.context, self.request), IDisplaySettings)
+        component = queryMultiAdapter((self.context, self.request),
+                                      IDisplaySettings)
         self.assertNotEqual(component, None)
 
     def test_implements_interface(self):
-        component = queryMultiAdapter((self.context, self.request), IDisplaySettings)
+        component = queryMultiAdapter((self.context, self.request),
+                                      IDisplaySettings)
         verifyClass(IDisplaySettings, component.__class__)
 
     def test_position(self):
-        settings = getMultiAdapter((self.context, self.request), IDisplaySettings)
+        settings = getMultiAdapter((self.context, self.request),
+                                   IDisplaySettings)
 
         self.assertEqual(settings.get_display(), None)
         settings.set_display({'top': 10, 'left': 30.7})
         self.assertEqual(settings.get_display(), {'top': 10, 'left': 30.7})
 
     def test_size(self):
-        settings = getMultiAdapter((self.context, self.request), IDisplaySettings)
+        settings = getMultiAdapter((self.context, self.request),
+                                   IDisplaySettings)
 
         self.assertEqual(settings.get_size(), None)
         settings.set_size({'width': 100, 'height': 50.3})
