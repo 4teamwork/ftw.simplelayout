c1a15ebc761cb90985fa6be346a891e46c6c007e
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-06-25 18:27:15 +0200

    fix pep8 violations

--- ftw/simplelayout/contents/paragraph.py
+++ ftw/simplelayout/contents/paragraph.py
@@ -7,33 +7,30 @@ from ftw.simplelayout.contents.interfaces import IParagraph
 
 from ftw.simplelayout import _
 
-schema = atapi.Schema((
+schema = atapi.Schema(
      atapi.BooleanField(
          'showTitle',
-          schemata='default',
-          default=0,
-          widget=atapi.BooleanWidget(
-              label=_(u"label_show_title", default="Show Title"),
-          )),
-    ),
+         schemata='default',
+         default=0,
+         widget=atapi.BooleanWidget(
+             label=_(u"label_show_title", default="Show Title"),
+         )),
     atapi.TextField(
         'text',
-         required=False,
-         searchable=True,
-         default_input_type='text/html',
-         default_output_type='text/html',
-         widget=atapi.RichWidget(
-             label=_(u'label_body_text', default=u'Body Text'),
-             rows=25,
-             filter_buttons = ('image', )),
-    ),
-)
+        required=False,
+        searchable=True,
+        default_input_type='text/html',
+        default_output_type='text/html',
+        widget=atapi.RichWidget(
+            label=_(u'label_body_text', default=u'Body Text'),
+            rows=25,
+            filter_buttons = ('image', ))),
+    )
 
 paragraph_schema = document.ATDocumentSchema.copy() + schema.copy()
 # paragraph_schema['excludeFromNav'].default = True
 # paragraph_schema['title'].required = False
 # paragraph_schema['description'].widget.visible = -1
-
 
 class Paragraph(document.ATDocument):
     security = ClassSecurityInfo()
