59fad21a87c95a78a8b6cefc84ce20333fdf2966
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-02-06 15:10:22 +0100

    Add contentpage and textblock builders.

--- ftw/simplelayout/tests/builders.py
+++ ftw/simplelayout/tests/builders.py
@@ -1,5 +1,6 @@
-from ftw.builder.archetypes import ArchetypesBuilder
 from ftw.builder import builder_registry
+from ftw.builder.archetypes import ArchetypesBuilder
+from ftw.builder.dexterity import DexterityBuilder
 
 
 class PageBuilder(ArchetypesBuilder):
@@ -12,3 +13,15 @@ class ParagraphBuilder(ArchetypesBuilder):
     portal_type = 'paragraph'
 
 builder_registry.register('sl paragraph', ParagraphBuilder)
+
+
+class ContenPageBuilder(DexterityBuilder):
+    portal_type = 'ftw.simplelayout.ContentPage'
+
+builder_registry.register('sl content page', ContenPageBuilder)
+
+
+class TextBlockBuilder(DexterityBuilder):
+    portal_type = 'ftw.simplelayout.TextBlock'
+
+builder_registry.register('sl textblock', TextBlockBuilder)
