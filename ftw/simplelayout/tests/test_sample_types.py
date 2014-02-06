cc43185034fd8926fd555b49cf7c155675364799
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-02-06 15:11:40 +0100

    Fix sample types tests.

--- ftw/simplelayout/tests/test_sample_types.py
+++ ftw/simplelayout/tests/test_sample_types.py
@@ -1,93 +1,53 @@
-from ftw.simplelayout.testing import FTW_SIMPLELAYOUT_INTEGRATION_TESTING
-from plone.app.testing import TEST_USER_NAME, TEST_USER_PASSWORD
-from plone.testing.z2 import Browser
+from ftw.builder import Builder
+from ftw.builder import create
+from ftw.simplelayout.testing import FTW_SIMPLELAYOUT_FUNCTIONAL_TESTING
+from ftw.testbrowser import browsing
+from ftw.testbrowser.pages import factoriesmenu
+from ftw.testbrowser.pages import plone
 from unittest2 import TestCase
-import transaction
-import StringIO
 
 
 class TestSampleTypes(TestCase):
 
-    layer = FTW_SIMPLELAYOUT_INTEGRATION_TESTING
+    layer = FTW_SIMPLELAYOUT_FUNCTIONAL_TESTING
 
     def setUp(self):
         super(TestSampleTypes, self).setUp()
 
-        portal = self.layer['portal']
-        self.portal_url = self.layer['portal'].portal_url()
+        self.page = create(Builder('sl content page').titled(u'A page'))
 
-        self.demo_folder = portal.get(portal.invokeFactory(
-            'Folder', 'demo-folder'))
-        self.page = self.demo_folder.get(self.demo_folder.invokeFactory(
-            'Page', 'demo-page'))
-        transaction.commit()
-
-        self.browser = Browser(self.layer['app'])
-        self.browser.handleErrors = False
-        self.browser.addHeader('Authorization', 'Basic %s:%s' % (
-            TEST_USER_NAME, TEST_USER_PASSWORD, ))
-
-    def test_add_page(self):
+    @browsing
+    def test_add_page(self, browser):
         # Page
-        self.browser.open(
-            self.portal_url + '/demo-folder/createObject?type_name=Page')
-        self.browser.getControl(name='title').value= u"A test page"
-        self.browser.getControl(name='form.button.save').click()
-        self.assertIn('A test page', self.browser.contents)
+        browser.login().visit()
+        factoriesmenu.add('ContentPage')
+        browser.fill({'Title': u'A test page'}).submit()
 
-    def test_add_paragraph_and_view(self):
-        self.browser.open(
-            self.page.absolute_url() + '/createObject?type_name=Paragraph')
-        self.browser.getControl(name='title').value= u"A test paragraph"
-        self.browser.getControl(name='text').value= u"Some text"
-        self.browser.getControl(name='showTitle:boolean').value = True
-        self.browser.getControl(name='form.button.save').click()
-        self.assertIn('A test paragraph', self.browser.contents)
-        self.assertIn('Some text', self.browser.contents)
+        self.assertEquals('A test page', plone.first_heading())
 
-    def test_paragraph_show_title(self):
-        self.browser.open(
-            self.page.absolute_url() + '/createObject?type_name=Paragraph')
-        self.browser.getControl(name='title').value= u"Not visible"
-        self.browser.getControl(name='text').value= u"Some other text"
-        self.browser.getControl(name='showTitle:boolean').value = False
-        self.browser.getControl(name='form.button.save').click()
-        self.assertNotIn('Not visible', self.browser.contents)
-        self.assertIn('Some other text', self.browser.contents)
+    @browsing
+    def test_add_textblock_with_title(self, browser):
+        browser.login().visit(self.page)
+        factoriesmenu.add('TextBlock')
+        browser.fill({'Title': u'A test textblock',
+                      'Text': u'Some text',
+                      'Show title': True}).submit()
 
-    def test_add_link_and_view(self):
-        self.browser.open(
-            self.page.absolute_url() + '/createObject?type_name=LinkBlock')
-        self.browser.getControl(name='title').value= u"A test link"
-        self.browser.getControl(
-            name='remoteUrl').value= u"http://www.4teamwork.ch"
-        self.browser.getControl(name='form.button.save').click()
-        self.assertIn('A test link', self.browser.contents)
-        self.assertIn('http://www.4teamwork.ch', self.browser.contents)
+        self.assertIn('Some text',
+                      browser.css('.block-view-wrapper').first.text)
+        self.assertEquals('A test textblock',
+                          browser.css('.block-view-wrapper h2').first.text)
 
-    def test_add_file_and_view(self):
-        self.browser.open(
-            self.page.absolute_url() + '/createObject?type_name=FileBlock')
-        self.browser.getControl(name='title').value= u"A test file"
-        ctrl = self.browser.getControl(name='file_file')
-        ctrl.add_file(StringIO.StringIO('File contents'),
-                     'text/plain', 'test.txt')
-        self.browser.getControl(name='form.button.save').click()
-        self.assertIn('A test file', self.browser.contents)
+    @browsing
+    def test_add_textblock_without_title(self, browser):
+        browser.login().visit(self.page)
+        factoriesmenu.add('TextBlock')
+        browser.fill({'Title': u'A test textblock',
+                      'Text': u'Some text',
+                      'Show title': False}).submit()
 
-    def test_add_image_and_view(self):
-        self.browser.open(
-            self.page.absolute_url() + '/createObject?type_name=ImageBlock')
-        self.browser.getControl(name='title').value= u"A test image"
-        ctrl = self.browser.getControl(name='image_file')
-        ctrl.add_file(StringIO.StringIO('Image contents'),
-                     'image/gif', 'test.gif')
-        self.browser.getControl(name='form.button.save').click()
-        self.assertIn('A test image', self.browser.contents)
-
-    def tearDown(self):
-        super(TestSampleTypes, self).tearDown()
-
-        portal = self.layer['portal']
-        portal.manage_delObjects(['demo-folder'])
-        transaction.commit()
+        self.assertIn('Some text',
+                      browser.css('.block-view-wrapper').first.text)
+        self.assertEquals(0,
+                          len(browser.css('.block-view-wrapper h2')),
+                          'Expect no textblock title')
