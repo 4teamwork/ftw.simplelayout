b45beb904be367e4fb7967832076d61e04996c78
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-03-14 11:29:53 +0100

    Add simplelayout control panel

---
+++ ftw/simplelayout/browser/controlpanel/simplelayout_controlpanel.py
@@ -0,0 +1,16 @@
+from ftw.simplelayout import _
+from ftw.simplelayout.interfaces import ISimplelayoutDefaultSettings
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.z3cform import layout
+from z3c.form import form
+
+
+class SimplelayoutDefaultSettingsForm(RegistryEditForm):
+    form.extends(RegistryEditForm)
+    schema = ISimplelayoutDefaultSettings
+
+SimplelayoutDefaultSettingsView = layout.wrap_form(
+    SimplelayoutDefaultSettingsForm,
+    ControlPanelFormWrapper)
+SimplelayoutDefaultSettingsView.label = _(u'Simplelayout default settings')
