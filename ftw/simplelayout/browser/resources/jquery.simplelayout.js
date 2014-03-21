e0f4733a173ca547978fba6de944fc88d467864d
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-03-21 15:53:09 +0100

    Move dnd upload functionality into its own private method.

--- ftw/simplelayout/browser/resources/jquery.simplelayout.js
+++ ftw/simplelayout/browser/resources/jquery.simplelayout.js
@@ -299,6 +299,76 @@ Private methods:
         });
     }
 
+    function dndupload($element, settings){
+      var counter = 0;
+      $element.on('dragenter', function (e){
+        e.stopPropagation();
+        e.preventDefault();
+
+        if (counter === 0) {
+          console.info('entered the page');
+        }
+        counter++;
+
+
+        if ($('.sl-add-block', $element).length === 0){
+          var files = e.originalEvent.dataTransfer.items;
+          $.each(files, function(index, file){
+            var $block = $(
+              '<div style="width:' + get_grid(settings) + 'px" ' +
+                   'class="sl-add-block '+ settings.blocks.slice(1) + '">'+
+                      '<span class="ui-icon ui-icon-document sl-controls-toggler">' +
+                      '<!-- --></span>' +
+
+                   '<div class="block-wrapper">' +
+                     '<div class="block-view-wrapper">' + file + '</div>' +
+                   '</div>' +
+              '</div>');
+            $element.prepend($block);
+          });
+
+          $element.masonry('reload');
+        }
+      });
+
+      $element.on('drop', function(e){
+        e.stopPropagation();
+        e.preventDefault();
+
+        var files = e.originalEvent.dataTransfer.files;
+        var $addblocks = $('.sl-add-block', $(this));
+
+        $.each(files, function(index, file){
+          var $block = $addblocks.eq(index);
+          $('.block-view-wrapper', $block).html('uploading... ' + file.name);
+
+          var data = new FormData();
+          data.append('image', file);
+          data.append('filename', file.name);
+
+          send_file_to_server(data, $block);
+        });
+
+      });
+
+      $element.on('dragleave', function (e) {
+
+        if (--counter === 0) {
+          console.info('leave');
+          $('.sl-add-block', $element).remove();
+          $element.masonry('reload');
+
+        }
+      });
+
+      $(document).on('dragover', function (e)
+      {
+        e.stopPropagation();
+        e.preventDefault();
+      });
+
+    }
+
     // Public functions
 
     var methods = {
@@ -334,78 +404,7 @@ Private methods:
                 blockcontrols($blocks);
                 addblock($this);
                 imagecontrols($blocks);
-
-                /******** THIS IS SOME FANCY IMAGE UPLOAD STUFF - JUST PLAYING ARROUND *************/
-
-                var counter = 0;
-                $this.on('dragenter', function (e){
-                  e.stopPropagation();
-                  e.preventDefault();
-
-                  if (counter === 0) {
-                    console.info('entered the page');
-                  }
-                  counter++;
-
-
-                  if ($('.sl-add-block', $this).length === 0){
-                    var files = e.originalEvent.dataTransfer.items;
-                    $.each(files, function(index, file){
-                      var $block = $(
-                        '<div style="width:' + get_grid(settings) + 'px" ' +
-                             'class="sl-add-block '+ settings.blocks.slice(1) + '">'+
-                                '<span class="ui-icon ui-icon-document sl-controls-toggler">' +
-                                '<!-- --></span>' +
-
-                             '<div class="block-wrapper">' +
-                               '<div class="block-view-wrapper">' + file + '</div>' +
-                             '</div>' +
-                        '</div>');
-                      $this.prepend($block);
-                    });
-
-                    $this.masonry('reload');
-                  }
-                });
-
-                $this.on('drop', function(e){
-                  e.stopPropagation();
-                  e.preventDefault();
-
-                  var files = e.originalEvent.dataTransfer.files;
-                  var $addblocks = $('.sl-add-block', $(this));
-
-                  $.each(files, function(index, file){
-                    var $block = $addblocks.eq(index);
-                    $('.block-view-wrapper', $block).html('uploading... ' + file.name);
-
-                    var data = new FormData();
-                    data.append('image', file);
-                    data.append('filename', file.name);
-
-                    send_file_to_server(data, $block);
-                  });
-
-                });
-
-                $this.on('dragleave', function (e) {
-
-                  if (--counter === 0) {
-                    console.info('leave');
-                    $('.sl-add-block', $this).remove();
-                    $this.masonry('reload');
-
-                  }
-                });
-
-                $(document).on('dragover', function (e)
-                {
-                  e.stopPropagation();
-                  e.preventDefault();
-                  //obj.css('border', '2px dotted #0B85A1');
-                });
-
-                /************************************************************/
+                dndupload($this, settings);
 
             });
         },
