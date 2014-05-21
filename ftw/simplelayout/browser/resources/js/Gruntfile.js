7293829da73b2e1c795fc4bb8bc9ef267a9d6ed3
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-05-21 18:26:35 +0200

    Add uglify and use a different progress logger.

--- ftw/simplelayout/browser/resources/js/Gruntfile.js
+++ ftw/simplelayout/browser/resources/js/Gruntfile.js
@@ -10,23 +10,39 @@ module.exports = function(grunt) {
                     interrupt: true,
                     debounceDelay: 250,
                 },
-                files: ['Gruntfile.js', 'src/*.js', 'tests/*.js'],
+                files: ['Gruntfile.js', './*.js', 'tests/*.js'],
                 tasks: ['mocha']
             }
         },
 
         mocha: {
-            all: {
+            test: {
                 src: ['tests/testrunner.html'],
+                options: {
+                    reporter: 'Progress',
+                },
             },
+            // all: {
+            //     src: ['tests/testrunner.html']
+            // },
             options: {
-                run: true
+                run: true,
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: './<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-mocha');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', ['mocha']);
 };
