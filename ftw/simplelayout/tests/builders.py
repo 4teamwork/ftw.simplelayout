4882bab55d1f3997b6144d0efe5384ea1535b9b4
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-07-02 12:21:02 +0200

    Remove old Archetypes builders.

--- ftw/simplelayout/tests/builders.py
+++ ftw/simplelayout/tests/builders.py
@@ -1,19 +1,6 @@
 from ftw.builder import builder_registry
-from ftw.builder.archetypes import ArchetypesBuilder
 from ftw.builder.dexterity import DexterityBuilder
 from StringIO import StringIO
-
-
-class PageBuilder(ArchetypesBuilder):
-    portal_type = 'Page'
-
-builder_registry.register('sl page', PageBuilder)
-
-
-class ParagraphBuilder(ArchetypesBuilder):
-    portal_type = 'paragraph'
-
-builder_registry.register('sl paragraph', ParagraphBuilder)
 
 
 class ContenPageBuilder(DexterityBuilder):
