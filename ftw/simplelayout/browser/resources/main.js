3e3e024af011c9206c4d5d8619b654f50003a9be
Kevin Bieri <k.bieri@4teamwork.ch>
2015-01-22 17:07:22 +0100

    Use prepoverlay for displaying form

--- ftw/simplelayout/browser/resources/main.js
+++ ftw/simplelayout/browser/resources/main.js
@@ -1,31 +1,77 @@
 $(document).ready(function() {
-  var target = $('#content-core');
 
-  var simplelayout = new Simplelayout();
-  var toolbox = new Toolbox({
-    layouts: [1, 2, 4]
+  var target = $('#content-core'),
+  componentRequest = $.get('./addable-blocks.json'),
+  toolbox,
+  simplelayout,
+  formHtml,
+  block,
+  dummyAnchor,
+  formUrl,
+  blockPosted = false,
+
+  createOverlay = function(target) {
+    dummyAnchor = $('<a>').attr('href', target);
+    dummyAnchor.prepOverlay({
+      subtype: 'ajax',
+      formselector: 'form',
+      noform: function(data, overlay) {
+        var uid = $('.sl-block-content', data).data('uid');
+        block.uid = uid;
+        block.content($('.sl-block-content', data).html());
+        return 'close';
+      },
+      afterpost: function() {
+        blockPosted = true;
+      },
+      closeselector: '[name="form.buttons.cancel"]',
+      config: {
+        onLoad: function() {
+          if (window.initTinyMCE) {
+            window.initTinyMCE(document);
+          }
+        },
+        onClose: function() {
+          if (!blockPosted) {
+            var layoutId = block.getElement().data('layoutId');
+            var columnId = block.getElement().data('columnId');
+            var blockId = block.getElement().data('blockId');
+            simplelayout.getLayoutmanager().deleteBlock(layoutId, columnId, blockId);
+          }
+          blockPosted = false;
+        }
+
+      }
+    });
+  };
+
+  componentRequest.done(function(data) {
+    toolbox = new Toolbox({
+      layouts: [1, 2, 4],
+      components: data
+    });
+    simplelayout = new Simplelayout({
+      width: '900px'
+    });
+    simplelayout.attachTo(target);
+    toolbox.attachTo(target);
+    simplelayout.attachToolbox(toolbox);
+
+    simplelayout.getToolbox().getElement().find('.sl-toolbox-component').on('dragstart', function(e) {
+      formUrl = $(e.target).data('form_url');
+    });
+
+    simplelayout.getLayoutmanager().getElement().on('blockInserted', function(event, layoutId, columnId, blockId) {
+      block = simplelayout.getLayoutmanager().getLayouts()[layoutId].getColumns()[columnId].getBlocks()[blockId];
+    });
+
+    simplelayout.getLayoutmanager().getElement().on('blocksCommitted', function(event, layoutId, columnId) {
+      createOverlay(formUrl);
+      dummyAnchor.trigger('click');
+    });
   });
 
-  simplelayout.attachTo(target);
-  toolbox.attachTo(target);
-  simplelayout.attachToolbox(toolbox);
-
-
-  var overlay = new Overlay();
-  simplelayout.getLayoutmanager().getElement().on('blockInserted', function(event, layoutId, columnId, blockId) {
-    var block = simplelayout.getLayoutmanager().getLayouts()[layoutId].getColumns()[columnId].getBlocks()[blockId];
-    simplelayout.getLayoutmanager().getElement().off('blocksCommitted').on('blocksCommitted', function(event, layoutId, columnId) {
-      $.post('http://localhost:8080/plattform/testpage/++add++ftw.simplelayout.TextBlock', function(data) {
-        var form = $('#form', data);
-        var html = $("<div>").append(form.clone()).html();
-        overlay.open(html);
-        $(document).off('submit').on('submit', form, function(e) {
-          e.preventDefault();
-          overlay.close();
-          console.log(block.getElement().data());
-          block.content('<p>content changed</p>');
-        });
-      });
-    });
+  componentRequest.fail(function(textStatus) {
+    console.error(textStatus);
   });
 });
