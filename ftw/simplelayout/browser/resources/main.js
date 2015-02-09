64b398aba683f4684a7e32169a3f1a08d760d5cf
Kevin Bieri <k.bieri@4teamwork.ch>
2015-02-09 11:11:57 +0100

    Code refactoring.

--- ftw/simplelayout/browser/resources/main.js
+++ ftw/simplelayout/browser/resources/main.js
@@ -1,77 +1,92 @@
-$(document).ready(function() {
+(function(global, $) {
+  "use strict";
 
-  var target = $('#content-core'),
-  componentRequest = $.get('./addable-blocks.json'),
-  toolbox,
-  simplelayout,
-  formHtml,
-  block,
-  dummyAnchor,
-  formUrl,
-  blockPosted = false,
+  $(function() {
 
-  createOverlay = function(target) {
-    dummyAnchor = $('<a>').attr('href', target);
-    dummyAnchor.prepOverlay({
-      subtype: 'ajax',
-      formselector: 'form',
-      noform: function(data, overlay) {
-        var uid = $('.sl-block-content', data).data('uid');
-        block.uid = uid;
-        block.content($('.sl-block-content', data).html());
-        return 'close';
+      var componentRequest = $.get("./addable-blocks.json"),
+      simplelayout,
+      block,
+      dummyAnchor,
+      formUrl,
+      blockPosted = false,
+
+      saveState = function(config) {
+        var saveRequest = $.post("./save_state", config);
+        saveRequest.done(function(data) {
+          block.element.data("uid", data.uid);
+        });
+        saveRequest.fail(function(data, textStatus) {
+          console.log(textStatus);
+        });
       },
-      afterpost: function() {
-        blockPosted = true;
-      },
-      closeselector: '[name="form.buttons.cancel"]',
-      config: {
-        onLoad: function() {
-          if (window.initTinyMCE) {
-            window.initTinyMCE(document);
+
+      createOverlay = function(target) {
+        dummyAnchor = $("<a>").attr("href", target);
+        dummyAnchor.prepOverlay({
+          subtype: "ajax",
+          formselector: "form",
+          noform: function(data) {
+            var uid = $(".sl-block-content", data).data("uid");
+            block.uid = uid;
+            block.content($(".sl-block-content", data).html());
+            return "close";
+          },
+          afterpost: function() {
+            blockPosted = true;
+            saveState(simplelayout.getLayoutmanager().serialize());
+          },
+          closeselector: "[name='form.buttons.cancel']",
+          config: {
+            onLoad: function() {
+              if (global.initTinyMCE) {
+                global.initTinyMCE(global.document);
+              }
+            },
+            onClose: function() {
+              if (!blockPosted) {
+                var layoutId = block.element.data("layoutId");
+                var columnId = block.element.data("columnId");
+                var blockId = block.element.data("blockId");
+                simplelayout.getLayoutmanager().deleteBlock(layoutId, columnId, blockId);
+              }
+              blockPosted = false;
+            }
+
           }
-        },
-        onClose: function() {
-          if (!blockPosted) {
-            var layoutId = block.getElement().data('layoutId');
-            var columnId = block.getElement().data('columnId');
-            var blockId = block.getElement().data('blockId');
-            simplelayout.getLayoutmanager().deleteBlock(layoutId, columnId, blockId);
-          }
-          blockPosted = false;
-        }
+        });
+      };
 
-      }
-    });
-  };
+    componentRequest.done(function(data) {
+      simplelayout = new global.Simplelayout({
+        source: "#simplelayout"
+      });
 
-  componentRequest.done(function(data) {
-    toolbox = new Toolbox({
-      layouts: [1, 2, 4],
-      components: data
-    });
-    simplelayout = new Simplelayout({
-      width: '900px'
-    });
-    simplelayout.attachTo(target);
-    toolbox.attachTo(target);
-    simplelayout.attachToolbox(toolbox);
+      var toolbox = new global.Toolbox({
+        layouts: [1, 2, 4],
+        components: data
+      });
 
-    simplelayout.getToolbox().getElement().find('.sl-toolbox-component').on('dragstart', function(e) {
-      formUrl = $(e.target).data('form_url');
-    });
+      toolbox.attachTo($("body"));
+      simplelayout.attachToolbox(toolbox);
+      simplelayout.getLayoutmanager().deserialize();
 
-    simplelayout.getLayoutmanager().getElement().on('blockInserted', function(event, layoutId, columnId, blockId) {
-      block = simplelayout.getLayoutmanager().getLayouts()[layoutId].getColumns()[columnId].getBlocks()[blockId];
+      simplelayout.getToolbox().element.find(".sl-toolbox-component").on("dragstart", function(e) {
+        formUrl = $(e.target).data("form_url");
+      });
+
+      simplelayout.getLayoutmanager().element.on("blockInserted", function(event, layoutId, columnId, blockId) {
+        block = simplelayout.getLayoutmanager().getBlock(layoutId, columnId, blockId);
+      });
+
+      simplelayout.getLayoutmanager().element.on("blocksCommitted", function() {
+        createOverlay(formUrl);
+        dummyAnchor.trigger("click");
+      });
     });
 
-    simplelayout.getLayoutmanager().getElement().on('blocksCommitted', function(event, layoutId, columnId) {
-      createOverlay(formUrl);
-      dummyAnchor.trigger('click');
+    componentRequest.fail(function(textStatus) {
+      console.error(textStatus);
     });
   });
 
-  componentRequest.fail(function(textStatus) {
-    console.error(textStatus);
-  });
-});
+}(window, jQuery));
