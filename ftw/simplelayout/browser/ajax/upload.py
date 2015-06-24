28a145cf8636e1a6a7c0fc83ca2052dc8e40ad4f
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2015-06-24 15:07:53 +0200

    Move js code from upload.py for upload.pt.

--- ftw/simplelayout/browser/ajax/upload.py
+++ ftw/simplelayout/browser/ajax/upload.py
@@ -30,31 +30,6 @@ class UploadForm(BrowserView):
         folder_url = ploneview.getCurrentFolderUrl()
         return '%s/@@quick_upload' % folder_url
 
-    def javascript(self):
-        return """
-            if (jQuery.browser.msie) jQuery("#settings").remove();
-            loadUploader = function() {
-              var ulContainer = jQuery('.quickupload.uploaderContainer');
-              ulContainer.each(function(){
-                  var uploadUrl =  jQuery('.uploadUrl', this).val();
-                  var uploadData =  'auto';
-                  var UlDiv = jQuery(this);
-                  jQuery.ajax({
-                             type: 'GET',
-                             url: uploadUrl,
-                             data: uploadData,
-                             dataType: 'html',
-                             contentType: 'text/html; charset=utf-8',
-                             success: function(html) {
-                                if (html.indexOf('quick-uploader') != -1) {
-                                    UlDiv.html(html);
-                                }
-                             } });
-              });
-            }
-            jQuery(document).ready(loadUploader);
-            """
-
     def render(self):
         response = {'content': self.template(),
                     'proceed': False}
