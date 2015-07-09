23feddcdcb0a8ebf02adac0085fb1923388112e2
Kevin Bieri <k.bieri@4teamwork.ch>
2015-07-09 14:12:12 +0200

    Fix eslink errors based on eslinter.

--- ftw/simplelayout/browser/resources/videoblock.js
+++ ftw/simplelayout/browser/resources/videoblock.js
@@ -1,52 +1,60 @@
-$(function(){
+(function(global) {
 
-  var youtube_player = {
-    playVideo: function(container, config) {
-        youtube_player.loadPlayer(container, config);
+  "use strict";
+
+  $(function() {
+
+    var youtubePlayer = {
+      playVideo: function(container, config) {
+        youtubePlayer.loadPlayer(container, config);
       },
 
-    loadPlayer: function(container, config) {
+      loadPlayer: function(container, config) {
 
-      var defaults = {
-        videoId: null,
-        playerVars: {
-          autoplay: 0,
-          controls: 0,
-          rel: 0,
-          showInfo: 0
-        }};
+        var defaults = {
+          videoId: null,
+          playerVars: {
+            autoplay: 0,
+            controls: 0,
+            rel: 0,
+            showInfo: 0
+          }
+        };
 
-        options = $.extend(defaults, config);
+        var options = $.extend(defaults, config);
 
-      new YT.Player(container, options);
-    }
-  };
-
-
-  initialize_youtube_api = function(callback){
-    var youtube_videos = $('.sl-youtube-video');
-    if (youtube_videos.length){
-
-      window.onYouTubePlayerAPIReady = function(){
-          youtube_videos.each(function(i, element){
-            var video = $(element);
-            youtube_player.playVideo(video.attr('id'), video.data('youtube'));
-          });
-       };
-
-      if (typeof YT === 'undefined' || typeof YT.Player === 'undefined'){
-        $.getScript('//www.youtube.com/iframe_api');
-      } else {
-        window.onYouTubePlayerAPIReady();
+        new global.YT.Player(container, options);
       }
-    }
-  };
+    };
 
 
-  $(document).on('onBeforeClose', '.overlay', function(){
-    initialize_youtube_api();
+    var initializeYoutubeApi = function() {
+      var youtubeVideos = $(".sl-youtube-video");
+      if (youtubeVideos.length) {
+
+        window.onYouTubePlayerAPIReady = function() {
+          youtubeVideos.each(function(i, element) {
+            var video = $(element);
+            youtubePlayer.playVideo(video.attr("id"), video.data("youtube"));
+          });
+        };
+
+        if (typeof YT === "undefined" || typeof global.YT.Player === "undefined") {
+          $.getScript("//www.youtube.com/iframe_api");
+        } else {
+          window.onYouTubePlayerAPIReady();
+        }
+      }
+    };
+
+
+    $(document).on("onBeforeClose", ".overlay", function() {
+      initializeYoutubeApi();
+    });
+
+    initializeYoutubeApi();
+
   });
 
-  initialize_youtube_api();
+})(window);
 
-});
