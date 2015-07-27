2fbfe6d058479da3428248f5eab663ef143dda7a
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2015-07-27 09:47:37 +0200

    Move addable_block_types function to utils.py

--- ftw/simplelayout/utils.py
+++ ftw/simplelayout/utils.py
@@ -1,7 +1,12 @@
 from ftw.simplelayout.interfaces import IBlockProperties
+from ftw.simplelayout.interfaces import ISimplelayoutBlock
+from plone.dexterity.interfaces import IDexterityFTI
 from plone.i18n.normalizer.interfaces import IIDNormalizer
+from Products.CMFCore.utils import getToolByName
 from zope.component import getUtility
 from zope.component import queryMultiAdapter
+from zope.component import queryUtility
+from zope.component.hooks import getSite
 
 
 def normalize_portal_type(portal_type):
@@ -14,3 +19,17 @@ def get_block_html(block):
                                    IBlockProperties)
     viewname = properties.get_current_view_name()
     return block.restrictedTraverse(viewname)()
+
+
+def get_block_types():
+    platform = getSite()
+    types_tool = getToolByName(platform, 'portal_types')
+    typeids = types_tool.objectIds()
+    typeids.sort()
+    for type_id in typeids:
+        dx_fti = queryUtility(IDexterityFTI, name=type_id)
+        if not dx_fti:
+            continue
+        else:
+            if ISimplelayoutBlock.__identifier__ in dx_fti.behaviors:
+                yield dx_fti
