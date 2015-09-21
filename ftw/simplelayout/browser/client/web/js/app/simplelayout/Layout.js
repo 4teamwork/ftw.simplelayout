7eb8f8ac1ac19eec6acc4f74e99e1eb518c83d1e
Kevin Bieri <k.bieri@4teamwork.ch>
2015-09-21 09:32:37 +0200

    Use range helper instead of column layer.

--- ftw/simplelayout/browser/client/web/js/app/simplelayout/Layout.js
+++ ftw/simplelayout/browser/client/web/js/app/simplelayout/Layout.js
@@ -1,108 +1,95 @@
-define(["app/simplelayout/Column", "app/simplelayout/EventEmitter"], function(Column, eventEmitter) {
+define([
+      "app/simplelayout/Block",
+      "app/simplelayout/EventEmitter",
+      "app/simplelayout/transactional",
+      "app/simplelayout/Element",
+      "app/helpers/template_range"
+    ],
+    function(
+      Block,
+      EventEmitter,
+      transactional,
+      Element) {
 
   "use strict";
 
-  function Layout(columns) {
+  var Layout = function(columns) {
     if (!(this instanceof Layout)) {
       throw new TypeError("Layout constructor cannot be called as a function.");
     }
-    if (!columns) {
-      throw new TypeError("Columns are not defined.");
-    }
 
-    var template = $.templates("<div class='sl-layout'></div>");
+    columns = columns || 4;
 
-    return {
+    var template = "<div class='sl-layout'>{{range start=1 end=columns ~columns=columns}}<div class='sl-column sl-col-{{:~columns}}'></div>{{/range}}</div>";
 
-      committed: false,
+    Element.call(this, template);
 
-      columns: {},
+    this.name = "layout";
 
-      toolbar: null,
+    this.create({ columns: columns });
 
-      create: function(id, container) {
-        this.element = $(template.render());
-        this.element.data("layoutId", id);
-        this.element.data("container", container);
-        for (var i = 0; i < columns; i++) {
-          var column = new Column(columns);
-          this.columns[i] = column;
-          var columnElement = column.create();
-          $.extend(columnElement.data(), { columnId: i, layoutId: id, container: container });
-          this.element.append(columnElement);
-        }
-        return this.element;
-      },
+    this.columns = columns;
 
-      insertBlock: function(columnId, content, type) { return this.columns[columnId].insertBlock({ content: content, type: type }); },
+    this.blocks = {};
 
-      commit: function() {
-        this.committed = true;
-        eventEmitter.trigger("layoutCommitted", [this]);
-      },
+    this.hasBlocks = function() { return Object.keys(this.blocks).length > 0; };
 
-      deleteBlock: function(columnId, blockId) { this.columns[columnId].deleteBlock(blockId); },
+    this.delete = function() { return this.parent.deleteLayout(this.id); };
 
-      commitBlocks: function(columnId) { this.columns[columnId].commitBlocks(); },
-
-      attachToolbar: function(toolbar) {
-        this.toolbar = toolbar;
-        this.element.append(toolbar.element);
-      },
-
-      getBlocks: function() {
-        return $.map(this.columns, function(column) {
-          return $.map(column.blocks, function(block) {
-            return block;
-          });
-        });
-      },
-
-      getCommittedBlocks: function() {
-        var committedBlocks = [];
-        for(var key in this.columns) {
-          committedBlocks = $.merge(this.columns[key].getCommittedBlocks(), committedBlocks);
-        }
-        return committedBlocks;
-      },
-
-      getInsertedBlocks: function() {
-        var insertedBlocks = [];
-        for(var key in this.columns) {
-          insertedBlocks = $.merge(this.columns[key].getInsertedBlocks(), insertedBlocks);
-        }
-        return insertedBlocks;
-      },
-
-      hasBlocks: function() {
-        var hasBlocks = false;
-        $.each(this.columns, function(columnIdx, column) {
-          if(column.hasBlocks()) {
-            hasBlocks = true;
-            return false;
-          }
-        });
-        return hasBlocks;
-      },
-
-      toJSON: function() { return { columns: this.columns }; },
-
-      deserialize: function() {
-        var self = this;
-        $(".sl-column", this.element).each(function(idx, e) {
-          e = $(e);
-          var column = self.columns[idx];
-          var data = column.element.data();
-          var stylingClass = column.element.attr("class");
-          column.element = e;
-          column.element.attr("class", stylingClass);
-          $.extend(column.element.data(), data);
-          column.deserialize();
-        });
-      }
-
+    this.insertBlock = function(content, type) {
+      var block = new Block(content, type);
+      block.parent = this;
+      block.data({ parent: this });
+      this.blocks[block.id] = block;
+      EventEmitter.trigger("blockInserted", [block]);
+      return block;
     };
-  }
+
+    this.deleteBlock = function(id) {
+      var block = this.blocks[id];
+      delete this.blocks[id];
+      EventEmitter.trigger("blockDeleted", [block]);
+      return block;
+    };
+
+    this.getCommittedBlocks = function() {
+      return $.grep($.map(this.blocks, function(block) { return block; }), function(block) {
+        return block.committed;
+      });
+    };
+
+    this.getInsertedBlocks = function() {
+      return $.grep($.map(this.blocks, function(block) { return block; }), function(block) {
+        return !block.committed;
+      });
+    };
+
+    this.moveBlock = function(block, target) {
+      EventEmitter.trigger("beforeBlockMoved", [block]);
+      this.deleteBlock(block.id);
+      block.parent = target;
+      block.data({ parent: target });
+      target.blocks[block.id] = block;
+      EventEmitter.trigger("blockMoved", [block]);
+      return this;
+    };
+
+    this.restore = function(restoreElement, restoreParent, restoreColumn, represents) {
+      var self = this;
+      this.columns = restoreColumn;
+      Layout.prototype.restore.call(this, restoreElement, restoreParent, represents);
+      this.commit();
+      $(".sl-block", restoreElement).each(function() {
+        self.insertBlock().restore(this, self, $(this).data().type, $(this).data().uid);
+      });
+    };
+
+    this.toJSON = function() { return { columns: this.columns, blocks: this.blocks }; };
+
+  };
+
+  transactional.call(Layout.prototype);
+  Element.call(Layout.prototype);
 
   return Layout;
 
