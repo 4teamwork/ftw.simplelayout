6b80d4cac09fc2d442ca91a7ae78fa1579d73d08
Kevin Bieri <k.bieri@4teamwork.ch>
2017-01-25 17:10:46 +0100

    Layout reload view: Implement generic data save option, instead of supporting only cssklasses.

--- ftw/simplelayout/browser/ajax/reloadlayoutview.py
+++ ftw/simplelayout/browser/ajax/reloadlayoutview.py
@@ -1,6 +1,7 @@
 from ftw.simplelayout.browser.provider import SimplelayoutRenderer
 from ftw.simplelayout.interfaces import IPageConfiguration
 from zope.publisher.browser import BrowserView
+from ftw.simplelayout.browser.ajax.utils import json_response
 import json
 
 
@@ -16,19 +17,30 @@ class ReloadLayoutView(BrowserView):
 
         self.name = self.data['name']
         self.layoutindex = int(self.data['layoutindex'])
-        self.klasses = self.data['cssklasses']
         self.set_layout_state()
-        return self.render_new_layout()
+        return json_response(self.request,
+                             {'content': self.render_new_layout()})
 
     def set_layout_state(self):
         page_conf = IPageConfiguration(self.context)
         self.new_state = page_conf.load()
+
+        current_config = self.new_state.get(self.name)[self.layoutindex].get(
+            'config', {})
+
+        for key, value in self.data['config'].items():
+            if key in current_config:
+                del current_config[key]
+            else:
+                current_config[key] = value
+
         self.new_state.get(
-            self.name)[self.layoutindex]['cssklasses'] = self.klasses
+            self.name)[self.layoutindex]['config'] = current_config
         page_conf.store(self.new_state)
 
     def render_new_layout(self):
         sl_renderer = SimplelayoutRenderer(self.context,
                                            self.new_state,
                                            self.name)
-        return sl_renderer.render_layout(index=self.layoutindex)
+        return sl_renderer.render_layout(index=self.layoutindex,
+                                         is_update=True)
