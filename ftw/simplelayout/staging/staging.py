456839fb56345875b95bde4669ec91e5453ffb22
Jonas Baumann <jone@jone.ch>
2018-07-12 09:05:20 +0200

    Implement applying working copy by copying field values.

--- ftw/simplelayout/staging/staging.py
+++ ftw/simplelayout/staging/staging.py
@@ -3,20 +3,29 @@ from Acquisition import aq_inner
 from Acquisition import aq_parent
 from BTrees.OOBTree import OOBTree
 from contextlib import contextmanager
+from ftw.simplelayout.configuration import columns_in_config
+from ftw.simplelayout.interfaces import IPageConfiguration
 from ftw.simplelayout.interfaces import ISimplelayoutBlock
 from ftw.simplelayout.staging.interfaces import IBaseline
 from ftw.simplelayout.staging.interfaces import IStaging
 from ftw.simplelayout.staging.interfaces import IWorkingCopy
+from operator import methodcaller
 from persistent.list import PersistentList
 from plone.app.uuid.utils import uuidToObject
+from plone.dexterity.interfaces import IDexterityContent
+from plone.dexterity.interfaces import IDexterityFTI
+from plone.dexterity.utils import getAdditionalSchemata
 from plone.uuid.interfaces import IUUID
+from Products.Archetypes.interfaces import IBaseContent
 from zope.annotation.interfaces import IAnnotations
 from zope.component import adapter
 from zope.component import getMultiAdapter
+from zope.component import getUtility
 from zope.interface import alsoProvides
 from zope.interface import implementer
 from zope.interface import Interface
 from zope.interface import noLongerProvides
+from zope.schema import getFieldsInOrder
 
 
 @implementer(IStaging)
@@ -33,6 +42,12 @@ def staging_lookup(context):
 @implementer(IStaging)
 @adapter(Interface, Interface)
 class Staging(object):
+
+    IGNORED_AT_FIELDS = (
+        'id',
+        'creation_date',
+        'modification_date',
+    )
 
     def __init__(self, context, request):
         self.context = context
@@ -69,7 +84,22 @@ class Staging(object):
         alsoProvides(self.context, IBaseline)
         alsoProvides(working_copy, IWorkingCopy)
         self._link(self.context, working_copy)
+        self._map_uuids(self.context, working_copy)
         return working_copy
+
+    def apply_working_copy(self):
+        """Apply the changes of the working copy back to the baseline and remove
+        the working copy.
+        """
+        if not self.is_working_copy():
+            raise ValueError('Adapted context must be a working copy.')
+
+        baseline = self.get_baseline()
+        working_copy = self.context
+        uuid_map = self._apply_children(working_copy, baseline,
+                                        condition=self.is_child_integrated)
+        self._update_simplelayout_page_state(working_copy, baseline, uuid_map)
+        self._delete_and_unlink_working_copy(baseline, working_copy)
 
     def discard_working_copy(self):
         """When the adapted context is the working copy, this method discards the
@@ -80,10 +110,15 @@ class Staging(object):
 
         baseline = self.get_baseline()
         working_copy = self.context
+        self._delete_and_unlink_working_copy(baseline, working_copy)
 
-        self._unlink(baseline, working_copy)
-        noLongerProvides(baseline, IBaseline)
-        aq_parent(aq_inner(working_copy)).manage_delObjects([working_copy.getId()])
+    def is_child_integrated(self, obj):
+        """Condition for deciding whether a child is "integrated" or not.
+        By "integrated" it's meant that the child is considered part of the content
+        and thus is copyied / applied along with the staged container.
+        In simplelayout this applies to simplelayout blocks by defualt.
+        """
+        return ISimplelayoutBlock.providedBy(obj)
 
     def _create_clone(self, obj, target_container):
         """When cloning an object, we want to make sure that we do not clone
@@ -93,20 +128,20 @@ class Staging(object):
         """
         source_container = aq_parent(aq_inner(obj))
         obj = aq_base(obj)
-        with self._cleanup_filter_tree(obj, ISimplelayoutBlock.providedBy), \
-             self._cleanup_filter_order(obj):
+        with self._cleanup_filter_tree(obj), self._cleanup_filter_order(obj):
             clipboard = source_container.manage_copyObjects([obj.getId()])
             copy_info, = target_container.manage_pasteObjects(clipboard)
         return target_container.get(copy_info['new_id'])
 
     @contextmanager
-    def _cleanup_filter_tree(self, obj, condition):
-        """Filter the child objects of the ``obj`` so that ``condition`` is ``True`` for
-        each child.
+    def _cleanup_filter_tree(self, obj):
+        """Filter the child objects of the ``obj`` so only children considered as "integrated"
+        are beeing kept.
         The context manager restores on exit.
         """
         original = obj._tree
-        obj._tree = OOBTree({key: value for (key, value) in obj._tree.items() if condition(value)})
+        obj._tree = OOBTree({key: value for (key, value) in obj._tree.items()
+                             if self.is_child_integrated(value)})
         try:
             yield
         finally:
@@ -128,6 +163,22 @@ class Staging(object):
             if annkey in annotations:
                 annotations[annkey] = original
 
+    def _map_uuids(self, baseline, working_copy):
+        """Map uuids of baseline objects and working copy objects.
+        The uuid of the baseline object is stored on the working copy object, recursively.
+        The method returns a map from baseline objects to their working copy objects.
+        """
+        baseline_to_workingcopy = {}
+
+        def handle(baseline_obj, working_copy_obj):
+            baseline_to_workingcopy[IUUID(baseline_obj)] = IUUID(working_copy_obj)
+            working_copy_obj._baseline_obj_uuid = IUUID(baseline_obj)
+            for name in working_copy_obj.objectIds():
+                handle(baseline_obj[name], working_copy_obj[name])
+
+        handle(baseline, working_copy)
+        return baseline_to_workingcopy
+
     def _link(self, baseline, working_copy):
         if not hasattr(baseline, '_working_copies'):
             baseline._working_copies = PersistentList()
@@ -138,3 +189,92 @@ class Staging(object):
     def _unlink(self, baseline, working_copy):
         del working_copy._baseline
         baseline._working_copies.remove(IUUID(working_copy))
+
+    def _delete_and_unlink_working_copy(self, baseline, working_copy):
+        self._unlink(baseline, working_copy)
+        noLongerProvides(baseline, IBaseline)
+        noLongerProvides(working_copy, IWorkingCopy)
+        aq_parent(aq_inner(working_copy)).manage_delObjects([working_copy.getId()])
+
+    def _apply_children(self, source_container, target_container, condition=None,
+                        uuid_map=None):
+        uuid_map = uuid_map or {}
+        uuid_map[IUUID(source_container)] = IUUID(target_container)
+        target_children_map = {IUUID(obj): obj for obj
+                               in filter(condition, target_container.objectValues())}
+        self._copy_field_values(source_container, target_container)
+
+        for source_obj in filter(condition, source_container.objectValues()):
+            target_uid = getattr(source_obj, '_baseline_obj_uuid', None)
+            if target_uid in target_children_map:
+                target_obj = target_children_map.pop(target_uid)
+                self._apply_children(source_obj, target_obj, uuid_map=uuid_map)
+            else:
+                self._move_new_obj(source_obj, target_container)
+
+        target_container.manage_delObjects(map(methodcaller('getId'),
+                                               target_children_map.values()))
+        return uuid_map
+
+    def _update_simplelayout_page_state(self, working_copy, baseline, uuid_map):
+        """Copy the simplelayout page state from the working copy to the baseline
+        and change the working copy block UUIDs to the existing baseline block UUIDs
+        unless new blocks are added.
+        """
+        config = IPageConfiguration(working_copy).load()
+        for column in columns_in_config(config):
+            for block in column['blocks']:
+                if block['uid'] in uuid_map:
+                    block['uid'] = uuid_map[block['uid']]
+
+        IPageConfiguration(baseline).store(config)
+
+    def _copy_field_values(self, source, target):
+        """Copy all fields values from "source" to "target".
+        """
+        if type(source) != type(target):
+            raise ValueError('Objects have differing classes ({!r} != {!r})'.format(
+                type(source), type(target)))
+
+        if IDexterityContent.providedBy(source):
+            return self._copy_dx_field_values(source, target)
+
+        if IBaseContent.providedBy(source):
+            return self._copy_at_field_values(source, target)
+
+        raise ValueError('Unsupported object type {!r}, neither AT nor DX.'.format(
+            source))
+
+    def _copy_dx_field_values(self, source, target):
+        for name, field, schemata in self._iter_fields(source.portal_type):
+            source_storage = schemata(source)
+            target_storage = schemata(target)
+            value = getattr(source_storage, name)
+            setattr(target_storage, field.getName(), value)
+
+    def _copy_at_field_values(self, source, target):
+        for source_field in source.Schema().values():
+            if source_field.__init__ in self.IGNORED_AT_FIELDS:
+                continue
+
+            # Re-fetch the field from the target schema since source and
+            # target schema may vary because of dynamice schema extenders.
+            target_field = target.Schema().getField(source_field.__name__)
+            value = source_field.getRaw(source)
+            target_field.set(target, value)
+
+    def _iter_fields(self, portal_type):
+        for schemata in self._iter_schemata_for_protal_type(portal_type):
+            for name, field in getFieldsInOrder(schemata):
+                if not getattr(field, 'readonly', False):
+                    yield (name, field, schemata)
+
+    def _iter_schemata_for_protal_type(self, portal_type):
+        fti = getUtility(IDexterityFTI, name=portal_type)
+        yield fti.lookupSchema()
+        for schema in getAdditionalSchemata(portal_type=portal_type):
+            yield schema
+
+    def _move_new_obj(self, obj, new_parent):
+        clipboard = aq_parent(aq_inner(obj)).manage_cutObjects([obj.getId()])
+        new_parent.manage_pasteObjects(clipboard)
--- ftw/simplelayout/tests/test_staging.py
+++ ftw/simplelayout/tests/test_staging.py
@@ -1,11 +1,13 @@
 from ftw.builder import Builder
 from ftw.builder import create
+from ftw.simplelayout.configuration import synchronize_page_config_with_blocks
 from ftw.simplelayout.interfaces import IPageConfiguration
 from ftw.simplelayout.staging.interfaces import IBaseline
 from ftw.simplelayout.staging.interfaces import IStaging
 from ftw.simplelayout.staging.interfaces import IWorkingCopy
 from ftw.simplelayout.testing import FTW_SIMPLELAYOUT_CONTENT_TESTING
 from ftw.testing import staticuid
+from plone.app.textfield.value import RichTextValue
 from plone.uuid.interfaces import IUUID
 from unittest2 import TestCase
 from zope.interface.verify import verifyObject
@@ -79,17 +81,122 @@ class TestWorkingCopy(TestCase):
                                            Builder('sl textblock').titled(u'Second')))
 
         self.assertEquals(
-            {'default': [{'cols': [{'blocks': [{'uid': 'baseline000000000000000000000002'},
-                                               {'uid': 'baseline000000000000000000000003'}]}]}]},
+            {'default': [{'cols': [{'blocks': [
+                {'uid': 'baseline000000000000000000000002'},
+                {'uid': 'baseline000000000000000000000003'}]}]}]},
             IPageConfiguration(baseline).load())
 
         with staticuid('workingcopy'):
             working_copy = IStaging(baseline).create_working_copy(self.portal)
 
         self.assertEquals(
-            {'default': [{'cols': [{'blocks': [{'uid': 'workingcopy000000000000000000001'},
-                                               {'uid': 'workingcopy000000000000000000002'}]}]}]},
+            {'default': [{'cols': [{'blocks': [
+                {'uid': 'workingcopy000000000000000000001'},
+                {'uid': 'workingcopy000000000000000000002'}]}]}]},
             IPageConfiguration(working_copy).load())
+
+    def test_apply_working_copy_copies_field_values_back_to_baseline(self):
+        bl_page = create(Builder('sl content page')
+                         .titled(u'original page title')
+                         .with_blocks(Builder('sl textblock')
+                                      .titled(u'original block title')
+                                      .having(text=RichTextValue(
+                                          u'<p>original block text</p>'))))
+
+        wc_page = IStaging(bl_page).create_working_copy(self.portal)
+        wc_page.setTitle(u'modified page title')
+        wc_block = wc_page.objectValues()[0]
+        wc_block.setTitle(u'modified block title')
+        wc_block.text = RichTextValue(u'<p>modified block text</p>')
+
+        IStaging(wc_page).apply_working_copy()
+        bl_block = bl_page.objectValues()[0]
+        self.assertEquals('modified page title', wc_page.Title())
+        self.assertEquals('modified block title', bl_block.Title())
+        self.assertEquals('<p>modified block text</p>', bl_block.text.output)
+
+    def test_applying_removes_blocks_from_baseline_when_removed_in_working_copy(self):
+        bl_page = create(Builder('sl content page').titled(u'Page')
+                         .with_blocks(Builder('sl textblock').titled(u'Foo'))
+                         .with_blocks(Builder('sl textblock').titled(u'Bar')))
+        create(Builder('sl content page').titled(u'Subpage').within(bl_page))
+        self.assertEquals(['foo', 'bar', 'subpage'], bl_page.objectIds())
+
+        wc_page = IStaging(bl_page).create_working_copy(self.portal)
+        self.assertEquals(['foo', 'bar'], wc_page.objectIds())
+        wc_page.manage_delObjects(['foo'])
+        self.assertEquals(['bar'], wc_page.objectIds())
+
+        IStaging(wc_page).apply_working_copy()
+        self.assertEquals(['bar', 'subpage'], bl_page.objectIds())
+
+    def test_applying_adds_new_blocks(self):
+        bl_page = create(Builder('sl content page').titled(u'Page'))
+        self.assertEquals([], bl_page.objectIds())
+
+        wc_page = IStaging(bl_page).create_working_copy(self.portal)
+        create(Builder('sl textblock').titled(u'Foo').within(wc_page))
+        IStaging(wc_page).apply_working_copy()
+
+        self.assertEquals(['foo'], bl_page.objectIds())
+
+    def test_files_in_filelistingblocks_are_synced(self):
+        bl_page = create(
+            Builder('sl content page').titled(u'Page')
+            .with_blocks(Builder('sl listingblock').titled(u'Downloads')
+                         .with_files(Builder('file').titled(u'One'),
+                                     Builder('file').titled(u'Two'))))
+
+        wc_page = IStaging(bl_page).create_working_copy(self.portal)
+        wc_page.downloads.one.setTitle('The first one')
+        create(Builder('file').titled(u'Three').within(wc_page.downloads))
+        wc_page.downloads.manage_delObjects(['two'])
+        IStaging(wc_page).apply_working_copy()
+
+        self.assertEquals('The first one', bl_page.downloads.one.Title())
+        self.assertEquals(['one', 'three'], bl_page.downloads.objectIds())
+
+    def test_sl_page_state_is_updated_when_applying_working_copy(self):
+        with staticuid('baseline'):
+            baseline = create(Builder('sl content page').titled(u'A page')
+                              .with_blocks(Builder('sl textblock').titled(u'First')))
+
+        self.assertEquals(
+            {'default': [{'cols': [{'blocks': [
+                {'uid': 'baseline000000000000000000000002'}]}]}]},
+            IPageConfiguration(baseline).load())
+
+        with staticuid('workingcopy'):
+            working_copy = IStaging(baseline).create_working_copy(self.portal)
+
+        self.assertEquals(
+            {'default': [{'cols': [{'blocks': [
+                {'uid': 'workingcopy000000000000000000001'}]}]}]},
+            IPageConfiguration(working_copy).load())
+
+        with staticuid('editing'):
+            create(Builder('sl textblock').titled(u'Second').within(working_copy))
+            synchronize_page_config_with_blocks(working_copy)
+
+        self.assertEquals(
+            {'default': [{'cols': [{'blocks': [
+                {'uid': 'workingcopy000000000000000000001'},
+                {'uid': 'editing0000000000000000000000001'}]}]}]},
+            IPageConfiguration(working_copy).load())
+
+        IStaging(working_copy).apply_working_copy()
+        self.assertEquals(
+            {'default': [{'cols': [{'blocks': [
+                {'uid': 'baseline000000000000000000000002'},
+                {'uid': 'editing0000000000000000000000001'}]}]}]},
+            IPageConfiguration(baseline).load())
+
+    def test_working_copy_is_removed_after_applying(self):
+        bl_page = create(Builder('sl content page').titled(u'A page'))
+        wc_page = IStaging(bl_page).create_working_copy(self.portal)
+        self.assertIn(wc_page.getId(), self.portal.objectIds())
+        IStaging(wc_page).apply_working_copy()
+        self.assertNotIn(wc_page.getId(), self.portal.objectIds())
 
     def test_discard_working_copy(self):
         baseline = create(Builder('sl content page').titled(u'A page'))
