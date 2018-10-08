f2e6036fda4158016a70fcf811ff4f3419722aee
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2018-10-08 18:40:32 +0200

    Add support for www.youtube-nocookie.com video urls.
    - More DSGVO compatible (only 5 cookies :-D).
    - As iframe - no youtplayer since that one loads a like 20 cookies.

--- ftw/simplelayout/contenttypes/browser/videoblock.py
+++ ftw/simplelayout/contenttypes/browser/videoblock.py
@@ -1,5 +1,6 @@
 from ftw.simplelayout.browser.blocks.base import BaseBlock
 from ftw.simplelayout.contenttypes.contents.videoblock import is_vimeo_url
+from ftw.simplelayout.contenttypes.contents.videoblock import is_youtube_nocookie_url
 from ftw.simplelayout.contenttypes.contents.videoblock import is_youtube_url
 from plone.uuid.interfaces import IUUID
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
@@ -8,11 +9,13 @@ import json
 
 
 VIMEO_PLAYER = "//player.vimeo.com/video/{0}"
+YOUTUBE_NO_COOKIE_PLAYER = 'https://www.youtube-nocookie.com/embed/{0}'
 
 
 class VideoBlockView(BaseBlock):
 
     youtube_template = ViewPageTemplateFile('templates/videoblock_youtube.pt')
+    youtube_nocookie_template = ViewPageTemplateFile('templates/videoblock_youtube_nocookie.pt')
     vimeo_template = ViewPageTemplateFile('templates/videoblock_vimeo.pt')
 
     template = None
@@ -20,6 +23,8 @@ class VideoBlockView(BaseBlock):
     def __call__(self):
         if is_youtube_url(self.context.video_url):
             self.template = self.youtube_template
+        elif is_youtube_nocookie_url(self.context.video_url):
+            self.template = self.youtube_nocookie_template
         elif is_vimeo_url(self.context.video_url):
             self.template = self.vimeo_template
         else:
@@ -38,11 +43,18 @@ class VideoBlockView(BaseBlock):
     def vimeo_player(self):
         return VIMEO_PLAYER.format(self.get_video_id())
 
+    def youtbube_no_cookie_player(self):
+        return YOUTUBE_NO_COOKIE_PLAYER.format(self.get_video_id())
+
     def get_video_id(self):
         if is_youtube_url(self.context.video_url):
             parsed_url = urlparse(self.context.video_url)
             return parsed_url.path[1:]
         elif is_vimeo_url(self.context.video_url):
+            parsed_url = urlparse(self.context.video_url)
+            path = parsed_url.path.split('/')
+            return path[-1]
+        elif is_youtube_nocookie_url(self.context.video_url):
             parsed_url = urlparse(self.context.video_url)
             path = parsed_url.path.split('/')
             return path[-1]
