081375b7ad7af50a050742e0ed917f609c1036f4
Tina Gerber <tina.gerber@4teamwork.ch>
2019-11-27 14:47:58 +0100

    Fix upgrade step for Plone 4.

--- ftw/simplelayout/contenttypes/upgrades/20191029182332_add_missing_image_scale/upgrade.py
+++ ftw/simplelayout/contenttypes/upgrades/20191029182332_add_missing_image_scale/upgrade.py
@@ -1,4 +1,8 @@
 from ftw.upgrade import UpgradeStep
+import pkg_resources
+
+
+IS_PLONE_5 = pkg_resources.get_distribution('Products.CMFPlone').version >= '5'
 
 
 class AddMissingImageScale(UpgradeStep):
@@ -6,4 +10,5 @@ class AddMissingImageScale(UpgradeStep):
     """
 
     def __call__(self):
-        self.install_upgrade_profile()
+        if IS_PLONE_5:
+            self.install_upgrade_profile()
