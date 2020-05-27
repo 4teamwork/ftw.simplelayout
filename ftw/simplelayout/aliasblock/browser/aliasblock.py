badc91b83b5a00b775aa7dee82b15b2ae9308f40
Matthias Osswald <m@osswald.li>
2020-05-27 20:55:10 +0200

    Fix/implement rendering the ftw.slider block.
    The ftw.slider block differs from the others. Therefore an additional condition is required. Also a fix is suggested in ftw.slider to actually add the interface prepared for the type to it. This means that the implementation in this commit will only work with the most recent code of ftw.slider.

--- ftw/simplelayout/aliasblock/browser/aliasblock.py
+++ ftw/simplelayout/aliasblock/browser/aliasblock.py
@@ -1,6 +1,15 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from ftw.simplelayout.browser.blocks.base import BaseBlock
+from ftw.simplelayout.interfaces import ISimplelayoutBlock
 from zExceptions import Unauthorized
+
+
+try:
+    from ftw.slider.interfaces import IContainer
+except ImportError:
+    class IContainer(object):
+        """Dummy class if ftw.slider isn't installed.
+        """
 
 
 class AliasBlockView(BaseBlock):
@@ -13,9 +22,12 @@ class AliasBlockView(BaseBlock):
 
     def get_referenced_block_content(self):
         """Returns the rendered simplayout block"""
-
         try:
-            view = self.referenced_page.restrictedTraverse('@@block_view')
-            return view.template()
+            if ISimplelayoutBlock.providedBy(self.referenced_page):
+                view = self.referenced_page.restrictedTraverse('@@block_view')
+                return view.template()
+            elif IContainer.providedBy(self.referenced_page):
+                view = self.referenced_page.restrictedTraverse('@@slider_view')
+                return view.template()
         except Unauthorized:
             return
