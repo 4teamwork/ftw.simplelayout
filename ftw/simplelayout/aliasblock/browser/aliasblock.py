b8bf2bd4e2dfa6b28e353317f65a7d9c6db1bb7f
Matthias Osswald <m@osswald.li>
2020-05-27 14:41:13 +0200

    Render block from other location.

--- ftw/simplelayout/aliasblock/browser/aliasblock.py
+++ ftw/simplelayout/aliasblock/browser/aliasblock.py
@@ -5,3 +5,13 @@ from ftw.simplelayout.browser.blocks.base import BaseBlock
 class AliasBlockView(BaseBlock):
 
     template = ViewPageTemplateFile('templates/aliasblock.pt')
+
+    def __init__(self, context, request):
+        super(AliasBlockView, self).__init__(context, request)
+        self.referenced_page = self.context.alias.to_object
+
+    def get_referenced_block_content(self):
+        """Returns the rendered simplayout block"""
+
+        view = self.referenced_page.restrictedTraverse('@@block_view')
+        return view.template()
