615820f3264e6be8e284938dad9acd2bade0911f
Matthias Osswald <m@osswald.li>
2020-05-27 15:15:50 +0200

    Test message if referenced block is missing.

--- ftw/simplelayout/tests/test_alias_block.py
+++ ftw/simplelayout/tests/test_alias_block.py
@@ -3,10 +3,12 @@ from ftw.builder import create
 from ftw.simplelayout.testing import FTW_SIMPLELAYOUT_CONTENT_TESTING
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import factoriesmenu
+from plone import api
 from unittest import TestCase
 from z3c.relationfield import RelationValue
 from zope.component import getUtility
 from zope.intid.interfaces import IIntIds
+import transaction
 
 
 class TestAliasBlockRendering(TestCase):
@@ -52,10 +54,10 @@ class TestAliasBlockRendering(TestCase):
         textblock = create(Builder('sl textblock')
                            .titled(u'\xc4s Bl\xf6ckli')
                            .within(self.page1))
-        alias = create(Builder('sl aliasblock')
-                       .having(alias=RelationValue(
-                           self.intids.getId(textblock)))
-                       .within(self.page2))
+        create(Builder('sl aliasblock')
+               .having(alias=RelationValue(
+                       self.intids.getId(textblock)))
+               .within(self.page2))
 
         browser.visit(self.page2)
         visit_link = browser.css('.sl-alias-block-visit-block').first.text
@@ -63,3 +65,20 @@ class TestAliasBlockRendering(TestCase):
 
         self.assertEqual(u'\U0001f517 Visit embedded block', visit_link)
         self.assertEqual(u'\xc4s Bl\xf6ckli', textblock_title)
+
+    @browsing
+    def test_has_message_if_referenced_block_was_deleted(self, browser):
+        textblock = create(Builder('sl textblock')
+                           .titled(u'\xc4s Bl\xf6ckli')
+                           .within(self.page1))
+        create(Builder('sl aliasblock')
+               .having(alias=RelationValue(
+                       self.intids.getId(textblock)))
+               .within(self.page2))
+
+        api.content.delete(obj=textblock, check_linkintegrity=False)
+        transaction.commit()
+        browser.visit(self.page2)
+        block_content = browser.css('.sl-alias-block').first.text
+
+        self.assertEqual(u'The embedded block does not exist.', block_content)
