a11b38a2a5ee12a9970385a26fa160560cbfedad
Matthias Osswald <m@osswald.li>
2020-05-29 10:43:45 +0200

    Use existing render method for getting block html.

--- ftw/simplelayout/aliasblock/browser/aliasblock.py
+++ ftw/simplelayout/aliasblock/browser/aliasblock.py
@@ -1,15 +1,6 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from ftw.simplelayout.browser.blocks.base import BaseBlock
-from ftw.simplelayout.interfaces import ISimplelayoutBlock
-from zExceptions import Unauthorized
-
-
-try:
-    from ftw.slider.interfaces import IContainer
-except ImportError:
-    class IContainer(object):
-        """Dummy class if ftw.slider isn't installed.
-        """
+from ftw.simplelayout.utils import get_block_html
 
 
 class AliasBlockView(BaseBlock):
@@ -22,12 +13,4 @@ class AliasBlockView(BaseBlock):
 
     def get_referenced_block_content(self):
         """Returns the rendered simplayout block"""
-        try:
-            if ISimplelayoutBlock.providedBy(self.referenced_page):
-                view = self.referenced_page.restrictedTraverse('@@block_view')
-                return view.template()
-            elif IContainer.providedBy(self.referenced_page):
-                view = self.referenced_page.restrictedTraverse('@@slider_view')
-                return view.template()
-        except Unauthorized:
-            return
+        return get_block_html(self.referenced_page)
