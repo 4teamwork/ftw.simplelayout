ab26d4b4d08767fa8e50d54931cec6bf6d3dbf7c
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2022-06-21 14:31:29 -0400

    Fix uninstall profile.

--- ftw/simplelayout/contenttypes/hooks.py
+++ ftw/simplelayout/contenttypes/hooks.py
@@ -35,6 +35,7 @@ def clean_plone5_registry(site):
     contains_objects.remove(u'ftw.simplelayout.ContentPage')
     contains_objects.remove(u'ftw.simplelayout.FileListingBlock')
     contains_objects.remove(u'ftw.simplelayout.GalleryBlock')
+    contains_objects.remove(u'ftw.simplelayout.MediaFolder')
     registry['plone.contains_objects'] = contains_objects
 
     displayed_types = list(registry['plone.displayed_types'])
